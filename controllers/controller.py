81f7d219918c64b91992137876acdfc8cd60ccd7
Stefano Binando <sbinando@gmail.com>
2021-11-05 15:56:46 +0000

    •Game.get_input_2_players split into 2 methods: Game.get_input_player1 Game.get_input_player1 •adds Game.get_computer_choices •adds route to 1_player.html •adds "back" to route to result page •completes navigation with 1_player and back button

--- controllers/controller.py
+++ controllers/controller.py
@@ -6,14 +6,17 @@ from models.player import *
 p1 = Player("a","b")
 p2 = Player("c","d")
 game = Game(p1, p2)
+# back = "/"
 
 @app.route('/')
 def index():
     return render_template('index.html', title='RPS-online')
 
+
 @app.route('/2_Players')
 def game1():
     return render_template('2players.html', title="Rock Paper Scissor")
+
 
 @app.route('/2_Players', methods=['POST'])
 def play_game1():
@@ -22,11 +25,30 @@ def play_game1():
     player2 = request.form['p2']
     choice_player2 = request.form['choice_p2']
 
-    game.get_input_2_players(player1, choice_player1, player2, choice_player2)
+    game.get_input_player1(player1, choice_player1)
+    game.get_input_player2(player2, choice_player2)
+    back = "2_Players"
+    return redirect(f'/{back}/{game.player1.choice}-{game.player2.choice}')
 
-    return redirect(f'/{game.player1.choice}/{game.player2.choice}')
 
-@app.route('/<choice1>/<choice2>')
-def result(choice1, choice2):
+
+@app.route('/1_Player')
+def game2():
+    return render_template('1player.html', title="Rock Paper Scissor")
+
+@app.route('/1_Player', methods=['POST'])
+def play_game2():
+    player1 = request.form['p1']
+    choice_player1 = request.form['choice_p1']
+    game.get_input_player1(player1, choice_player1)
+    game.get_computer_choices()
+
+    back = "1_Player"
+    # print(back)
+    return redirect(f'/{back}/{game.player1.choice}-{game.player2.choice}')
+
+
+@app.route('/<back>/<choice1>-<choice2>')
+def result(choice1, choice2, back):
     winner = game.get_winner()
-    return render_template('result.html', title="Rock Paper Scissor", winner = winner)
+    return render_template('result.html', title="Rock Paper Scissor", winner = winner, back = back)
--- models/game.py
+++ models/game.py
@@ -1,19 +1,25 @@
-from os import name
 from models.player import Player
+from random import randint
 
 class Game:
     def __init__(self, player1, player2):
         self.player1 = player1
         self.player2 = player2
 
-    def get_input_2_players(self, name1, choice1, name2, choice2):
+    def get_input_player1(self, name1, choice1):
         self.player1.name = name1
         self.player1.choice = choice1
+
+    def get_input_player2(self, name2, choice2):
         self.player2.name = name2
         self.player2.choice = choice2
 
-    def get_winner(self):
+    def get_computer_choices(self,):
+        self.player2.name = "HAL 9000"
+        choices = ["scissors", "rock", "paper"]
+        self.player2.choice = choices[randint(0, 2)]
 
+    def get_winner(self):
         winner = None
         winner = self.player1 if (
             (self.player1.choice[0] == "s" and self.player2.choice[0] == "p") or
